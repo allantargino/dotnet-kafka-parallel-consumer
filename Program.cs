47c219422bc2ffd3af3e59c24e09f435580b2bdd
Allan Targino <13934447+allantargino@users.noreply.github.com>
2022-05-27 11:23:18 -0300

    dev env

--- Program.cs
+++ Program.cs
@@ -10,30 +10,31 @@ IHost host = Host.CreateDefaultBuilder(args)
         o.TimestampFormat = "hh:mm:ss:fff ";
     }))
     .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-        services.AddSingleton<IConsumer<string, string>>(svc =>
-        {
-            var config = new ConsumerConfig
-            {
-                BootstrapServers = "localhost:9092",
-                GroupId = "csharp-consumer",
-                EnableAutoCommit = false,
-                StatisticsIntervalMs = 5000,
-                SessionTimeoutMs = 6000,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnablePartitionEof = false,
+   {
+       services.AddHostedService<SerialWorker>();
+
+       services.AddSingleton(svc =>
+       {
+           var config = new ConsumerConfig
+           {
+               BootstrapServers = "localhost:9092",
+               GroupId = "csharp-consumer",
+               EnableAutoCommit = false,
+               StatisticsIntervalMs = 5000,
+               SessionTimeoutMs = 6000,
+               AutoOffsetReset = AutoOffsetReset.Earliest,
+               EnablePartitionEof = false,
                 // A good introduction to the CooperativeSticky assignor and incremental rebalancing:
                 // https://www.confluent.io/blog/cooperative-rebalancing-in-kafka-streams-consumer-ksqldb/
                 PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
-            };
+           };
 
-            return new ConsumerBuilder<string, string>(config)
-                        .Build();
-        });
+           return new ConsumerBuilder<string, string>(config)
+                       .Build();
+       });
 
-        services.AddSingleton<IProcessor<string, string>, Processor<string,string>>();
-    })
+       services.AddSingleton<IProcessor<string, string>, Processor<string, string>>();
+   })
     .Build();
 
 await host.RunAsync();
